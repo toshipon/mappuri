c20f0b88b6ecdb4c81df5d4999ff2565e69bd256
Shawn Smith <shawnpsmith@gmail.com>
2013-06-26 23:29:05 +0900

    update structs, add handler for getting an outing from db

--- mappuri.go
+++ mappuri.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
 	"net/http"
 	"net/url"
 )
 
+const mongoDbUrl = "localhost"
+
 type Place struct {
-	Name    string
-	MapLink url.URL
+	Id             bson.ObjectId `bson:"_id"`
+	Name           string
+	MapLink        *url.URL `bson:",omitempty"`
+	FoursquareLink *url.URL `bson:",omitempty"`
+	Website        *url.URL `bson:",omitempty"`
 }
 
 type Outing struct {
+	Id     bson.ObjectId `bson:"_id"`
+	Name   string
 	Places []Place
 }
 
@@ -27,9 +37,35 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func loadOuting(outingId string) (*Outing, error) {
+	session, err := mgo.Dial(mongoDbUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	c := session.DB("mappuri").C("outings")
+	outing := Outing{}
+	err = c.FindId(bson.ObjectIdHex(outingId)).One(&outing)
+	if err != nil {
+		return nil, err
+	}
+	return &outing, nil
+}
+
+func OutingHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	outingId := vars["outingId"]
+	outing, err := loadOuting(outingId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(outing)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/outings/{outingId}", OutingHandler)
 	http.Handle("/", r)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.ListenAndServe(":8080", nil)
