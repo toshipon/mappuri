12e4ba3f7a4538cf96e54b2c5980cb80f0812533
Shawn Smith <shawnpsmith@gmail.com>
2013-07-02 23:30:37 +0900

    use gorilla pat, add list outings handler and create outing handler

--- mappuri.go
+++ mappuri.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/gorilla/pat"
+	"github.com/gorilla/schema"
 	"html/template"
 	"io/ioutil"
 	"labix.org/v2/mgo"
@@ -13,6 +14,8 @@ import (
 )
 
 const mongoDbUrl = "localhost"
+
+var decoder = schema.NewDecoder()
 
 type Place struct {
 	Id             bson.ObjectId `bson:"_id"`
@@ -53,9 +56,23 @@ func loadOuting(outingId string) (*Outing, error) {
 	return &outing, nil
 }
 
-func OutingHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	outingId := vars["outingId"]
+func loadOutings() (*[]Outing, error) {
+	session, err := mgo.Dial(mongoDbUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	c := session.DB("mappuri").C("outings")
+	outings := []Outing{}
+	err = c.Find(nil).All(&outings)
+	if err != nil {
+		return nil, err
+	}
+	return &outings, nil
+}
+
+func showOuting(w http.ResponseWriter, r *http.Request) {
+	outingId := r.URL.Query().Get(":outingId")
 	outing, err := loadOuting(outingId)
 	if err != nil {
 		log.Fatal(err)
@@ -64,11 +81,29 @@ func OutingHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, outing)
 }
 
+func listOutings(w http.ResponseWriter, r *http.Request) {
+	outing, err := loadOutings()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, _ := template.ParseFiles("templates/outing_detail.html")
+	t.Execute(w, outing)
+}
+
+func createOuting(w http.ResponseWriter, r *http.Request) {
+    outing := Outing{}
+	r.ParseForm()
+	decoder.Decode(outing, r.PostForm)
+	fmt.Println(outing)
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/outings/{outingId}", OutingHandler)
-	http.Handle("/", r)
+	r := pat.New()
+	r.Get("/", HomeHandler)
+	r.Get("/outings/{outingId}", http.HandlerFunc(showOuting))
+	r.Get("/outings", http.HandlerFunc(listOutings))
+	r.Post("/outings", http.HandlerFunc(createOuting))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
